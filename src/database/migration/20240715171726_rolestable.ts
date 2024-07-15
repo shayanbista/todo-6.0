4ace468d9f11c4017115205e473c4b4d91bc8395
Shayan bista <shayanbista94@gmail.com>
2024-07-15 23:08:04 +0545

    roles and permission add

---
+++ src/database/migration/20240715171726_rolestable.ts
@@ -0,0 +1,37 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = "roles";
+
+/**
+ * Create table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.bigIncrements();
+    table.string("roleName", 100).notNullable();
+
+    table.timestamp("created_at").notNullable().defaultTo(knex.raw("now()"));
+
+    table.timestamp("updated_at").nullable();
+
+    table
+      .bigInteger("updated_by")
+      .unsigned()
+      .references("id")
+      .inTable(TABLE_NAME)
+      .nullable();
+  });
+}
+
+/**
+ * Drop table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
