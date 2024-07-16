09d407b0db734b00e4673b2c64c24496a7105786
Shayan bista <shayanbista94@gmail.com>
2024-07-16 09:56:22 +0545

    assignment add

---
+++ src/database/migration/20240716040050_etable.ts
@@ -0,0 +1,57 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = "userRoles";
+
+/**
+ * Create table userRoles.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.bigIncrements();
+
+    table
+      .bigInteger("userId")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users");
+
+    table
+      .bigInteger("roleId")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("roles");
+    
+    table.timestamp("created_at").notNullable().defaultTo(knex.raw("now()"));
+
+    table
+      .bigInteger("created_by")
+      .unsigned()
+      .nullable()
+      .references("id")
+      .inTable(TABLE_NAME);
+
+    table.timestamp("updated_at").nullable();
+
+    table
+      .bigInteger("updated_by")
+      .unsigned()
+      .references("id")
+      .inTable(TABLE_NAME)
+      .nullable();
+  });
+}
+
+/**
+ * Drop table userRoles.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
